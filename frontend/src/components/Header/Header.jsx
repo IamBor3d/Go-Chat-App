9e24147f3c291a34c1bba544c6697d8a7c985651
Aaman Ali <aaman.ali31106@gmail.com>
2026-07-01 17:08:57 +0100

    Add Header Component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.scss"
+const Header = () => {
+    return (
+        <div className="text-center">
+            <h2>RealTime Chat</h2>
+        </div>
+    );
+};
+
+export default Header;
