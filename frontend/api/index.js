8a787f7fb216ea01564f32c5d1d20d51cc6e59d4
Aaman Ali <aaman.ali31106@gmail.com>
2026-07-01 14:25:12 +0100

    Add websocket support to front end

---
+++ frontend/api/index.js
@@ -0,0 +1,34 @@
+
+//Create websocket
+var socket = new WebSocket("ws://localhostL:8080/ws");
+
+//Listen for events on a socket 
+let connect = () => {
+
+    console.log("Attempting Connection");
+
+    socket.onopen = () => {
+        console.log("Successfully opened");
+    };
+
+    socket.onmessage = msg => {
+        console.log(msg);
+    };
+
+    socket.onclose = event => {
+        console.log("Closed Connection: ", event);
+    };
+
+    socket.onerror = err =>{
+        console.log("Server Error", err);
+    };
+
+};
+
+//send a message to backend
+let sendMsg = msg => {
+    console.log("sending msg:", msg);
+    socket.send(msg);
+};
+
+export { connect, sendMsg}
