0dc7d033a6fec91801697bcd803a30d567268429
Aaman Ali <aaman.ali31106@gmail.com>
2026-07-02 14:18:07 +0100

    Add client support to main.go

--- backend/main.go
+++ backend/main.go
@@ -8,24 +8,32 @@ import (
 	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Host)
 
-	ws, err := websocket.Upgrade(w, r)
+	conn, err := websocket.Upgrade(w, r)
 
 	if err != nil {
 		log.Println(w, "%+V\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setUpRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "simpleServer")
+
+	pool := websocket.NewPool()
+	go pool.Start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
 
-	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
